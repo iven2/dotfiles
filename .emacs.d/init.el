6d9cdc277734bab1bd6ff8ffe8ac99e4d5aaa640
xcui <xcui@m660.(none)>
2013-10-23 03:48:08 -0400

    initial commit with default dot configuration files

---
+++ .emacs.d/init.el
@@ -0,0 +1,19 @@
+;; show line numbers
+(global-linum-mode t)
+
+;; c++ mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+;; don't indent namespace
+(c-set-offset 'innamespace 0)
+
+;; ido mode... should read about this
+(require 'ido)
+(ido-mode t)
+;; disable auto-save & backup files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+
+;; highlight matching parenthesis
+(show-paren-mode 1)
+(setq show-paren-delay 0)
