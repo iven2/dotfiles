df7ad269081be8e328322fe25c62142f3c2c5e5b
xcui <cuixu66@hotmail.com>
2013-12-23 01:53:58 -0500

    fix ruby indentation problem

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -76,3 +76,29 @@
 ;;; rhtml is a html syntax highlighting tool for ruby on rails
 (add-to-list 'load-path "~/dotfiles/.emacs.d/plugins/rhtml")
 (require 'rhtml-mode)
+
+
+;; added to fix ruby indentation problem after type the newline
+;; source:
+;; http://stackoverflow.com/questions/4412739/emacs-ruby-mode-indentation-behavior
+(defadvice ruby-indent-line (after line-up-args activate)
+  (let (indent prev-indent arg-indent)
+    (save-excursion
+      (back-to-indentation)
+      (when (zerop (car (syntax-ppss)))
+        (setq indent (current-column))
+        (skip-chars-backward " \t\n")
+        (when (eq ?, (char-before))
+          (ruby-backward-sexp)
+          (back-to-indentation)
+          (setq prev-indent (current-column))
+          (skip-syntax-forward "w_.")
+          (skip-chars-forward " ")
+          (setq arg-indent (current-column)))))
+    (when prev-indent
+      (let ((offset (- (current-column) indent)))
+        (cond ((< indent prev-indent)
+               (indent-line-to prev-indent))
+              ((= indent prev-indent)
+               (indent-line-to arg-indent)))
+        (when (> offset 0) (forward-char offset))))))
