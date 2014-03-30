848597bd595b734573c57682e4a07a76b2202c6e
cuixu66 <cuixu66@hotmail.com>
2014-03-30 13:00:34 -0400

    changed erb file default mode to web

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -164,8 +164,9 @@
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-;; erb file is better handled with erb config above
-;; (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+;; erb file is better handled with web-mode because rhtml does not indent
+;; javascript correctly
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
