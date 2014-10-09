9b949891b79abdbc24f5530984b37e26a8bb1ddd
cuixu <cuixu66@hotmail.com>
2014-10-08 21:37:53 -0400

    Remove the useless scratch and message buffers for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -248,6 +248,38 @@
  ;; Disabling strict-inconsistent-return warning for js2 mode(javascript mode)
  '(js2-strict-inconsistent-return-warning nil))
 
-
-;; TO avoid .# files that is created by emacs:
+;; To avoid .# files that is created by emacs:
 (setq create-lockfiles nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; kill the useless *Messages* and *scratch* buffers ;;;;;;;;;;
+;; Makes *scratch* empty.
+(setq initial-scratch-message "")
+
+;; Removes *scratch* from buffer after the mode has been set.
+(defun remove-scratch-buffer ()
+  (if (get-buffer "*scratch*")
+      (kill-buffer "*scratch*")))
+(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+
+;; Removes *messages* from the buffer.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
+;; Removes *Completions* from buffer after you've opened a file.
+(add-hook 'minibuffer-exit-hook
+          '(lambda ()
+             (let ((buffer "*Completions*"))
+               (and (get-buffer buffer)
+                    (kill-buffer buffer)))))
+
+;; Don't show *Buffer list* when opening multiple files at the same time.
+(setq inhibit-startup-buffer-menu t)
+
+;; Show only one active window when opening multiple files at the same time.
+(add-hook 'window-setup-hook 'delete-other-windows)
+;; End of kill the useless *Messages* and *scratch* buffers ;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; No more typing the whole yes or no. Just y or n will do.
+(fset 'yes-or-no-p 'y-or-n-p)
